d3bcb1ea88cc171cc77d7a6c59cf5db0de33d083
niko124a <56532129+niko124a@users.noreply.github.com>
2020-01-06 12:36:59 +0100

    minus should work now
    Ohhhhhh yeeeees

--- Lommeregner H1/Program.cs
+++ Lommeregner H1/Program.cs
@@ -5,60 +5,44 @@ namespace Lommeregner_H1
     class Program
     {
 
-        static double minus()
+        public static double minus(double result, double num)
         {
-
-            double num1;
-            double num2;
-            double result;
-            double operators;
-
-            double number = double.Parse(Console.ReadLine());
-
-            num1 = number;
-            operators = number;
-            num2 = number;
-            result = num1 - num2;
-            return result;
+            return result - num;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            minus();
 
-            static void Main()
+
+            Console.Write("Number 1: ");
+            double result = Convert.ToDouble(Console.ReadLine());
+
+            while (true)
             {
-                Console.Write("Number 1: ");
-                double result = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Operator type: ");
+                string OpType = Console.ReadLine();
 
-                while (true)
+                Console.Write("Number 2: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                switch (OpType)
                 {
-                    Console.Write("Operator type: ");
-                    string OpType = Console.ReadLine();
-
-                    Console.Write("Number 2: ");
-                    double numtwo = Convert.ToDouble(Console.ReadLine());
-
-                    switch (OpType)
-                    {
-                        case "+":
-                            //result += Add
-                            break;
-                        case "-":
-                            //result += Subtract
-                            break;
-                        case "*":
-                            //result += Addition
-                            break;
-                        case "/":
-                            //result += Division
-                            break;
-                        default:
-                            Console.WriteLine($"Your result is {result}");
-                            Console.ReadKey();
-                            return;
-                    }
+                    case "+":
+                        //result += Add
+                        break;
+                    case "-":
+                        result += minus(result, num2);
+                        break;
+                    case "*":
+                        //result += Addition
+                        break;
+                    case "/":
+                        //result += Division
+                        break;
+                    default:
+                        Console.WriteLine($"Your result is {result}");
+                        Console.ReadKey();
+                        return;
                 }
             }
         }
