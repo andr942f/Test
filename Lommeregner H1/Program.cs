af843d56ef5023dc0a3eb008f6440aeb2d5db602
niko124a <56532129+niko124a@users.noreply.github.com>
2020-01-06 12:27:28 +0100

    something
    yes

--- Lommeregner H1/Program.cs
+++ Lommeregner H1/Program.cs
@@ -4,9 +4,28 @@ namespace Lommeregner_H1
 {
     class Program
     {
+
+        static double minus()
+        {
+
+            double num1;
+            double num2;
+            double result;
+            double operators;
+
+            double number = double.Parse(Console.ReadLine());
+
+            num1 = number;
+            operators = number;
+            num2 = number;
+            result = num1 - num2;
+            return result;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            minus();
         }
     }
 }
