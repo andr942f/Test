5ba9ca55b28597e90cf1f76fa45c43b82eaaedcb
DanielSimonsen90 <danielsimonsen90@gmail.com>
2020-01-06 12:21:08 +0100

    Created GUI
    I hope this works amazingly UwU

--- Lommeregner H1/Program.cs
+++ Lommeregner H1/Program.cs
@@ -4,9 +4,39 @@ namespace Lommeregner_H1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Number 1: ");
+            double result = Convert.ToDouble(Console.ReadLine());
+
+            while (true)
+            {
+                Console.Write("Operator type: ");
+                string OpType = Console.ReadLine();
+
+                Console.Write("Number 2: ");
+                double numtwo = Convert.ToDouble(Console.ReadLine());
+
+                switch (OpType)
+                {
+                    case "+":
+                        //result += Add
+                        break;
+                    case "-":
+                        //result += Subtract
+                        break;
+                    case "*":
+                        //result += Addition
+                        break;
+                    case "/":
+                        //result += Division
+                        break;
+                    default:
+                        Console.WriteLine($"Your result is {result}");
+                        Console.ReadKey();
+                        return;
+                }
+            }
         }
     }
 }
