db329509f6eaae035c502362d716e27d67865f22
niko124a <56532129+niko124a@users.noreply.github.com>
2020-01-06 11:23:13 +0100

    Added Lommeregner
    Yes

---
+++ Lommeregner H1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lommeregner_H1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
